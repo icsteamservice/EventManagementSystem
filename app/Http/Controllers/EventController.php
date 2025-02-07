0e2e8e08245fc7b1315afcb685f28a7a70b859eb
unknown <saidulislam2822@gmail.com>
2025-02-07 17:06:17 +0600

    copilot tested

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -89,7 +89,6 @@ class EventController extends Controller
      */
     public function show($id)
     {
-        
         $event = Event::with(['vendor', 'venue', 'category', 'artist'])->findOrFail($id);
         return view('event.show', compact('event'));
     }
